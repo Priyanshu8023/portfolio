ecd9d94cf1c0e985bd775497045ba49de9cff41d
Priyanshu8023 <priyanshukumar8023@gmail.com>
2025-12-19 01:27:45 +0530

    Add Hero Section

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from "react";
+
+const Hero: React.FC = () => {
+  return (
+    <>
+
+      <div className="flex justify-center text-center">
+        <h1 className="text-7xl flex justify-center text-center">
+          PRIYANSHU KUMAR
+        </h1>
+      </div>
+      <div className="flex justify-center text-center">
+        <h2 className="text-xl flex justify-center text-center">
+          Full Stack Developer
+        </h2>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
