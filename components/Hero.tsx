de018434b911fedbfc2d70a8f5c099556444be87
Priyanshu8023 <priyanshukumar8023@gmail.com>
2025-12-19 12:16:28 +0530

    Create a Basic HeroSection

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,33 @@
 "use client"
-import React from "react";
 
-const Hero: React.FC = () => {
+import { anton , unbounded } from "@/lib/font"
+// import { useHeroAnimaion } from "@/hooks/useHeroAnimaion"
+
+export default function Hero() {
+  // const textRef = useHeroAnimation();
   return (
     <>
+       <div className="absolute insert-0 flex flex-col items-center pt-40 md:pt-40 text-white">
+          <div className="flex flex-col gap-2 items-center justify-center text-center">
+            <h1 className={`${anton.className} text-6xl md:text-8xl leading-tight`} >
+              PRIYANSHU KUMAR
+            </h1>
 
-      <div className="flex justify-center text-center">
-        <h1 className="text-7xl flex justify-center text-center">
-          PRIYANSHU KUMAR
-        </h1>
-      </div>
-      <div className="flex justify-center text-center">
-        <h2 className="text-xl flex justify-center text-center">
-          Full Stack Developer
-        </h2>
-      </div>
+            <h3 className={`${unbounded.className} text-xl md:text-4xl leading-tight`}>
+              Full Stack Developer
+            </h3>
+          </div>
+          <div className={`${unbounded.className} text-l md:text-2xl flex text-center max-w-[500px] pt-30 md:pt-40 px-1 md:px-0`}>
+            {/* <p ref={textRef}>
+                CRafting Seamless experiences,
+                <br /> one line at a time.
+            </p> */}
+            <p >
+              Crafting Seamless experiences,
+              <br /> one line at a time.
+            </p>
+          </div>
+       </div>
     </>
   );
-};
-
-export default Hero;
+}
